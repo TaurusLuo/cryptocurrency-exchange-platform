012f7787595eeb212aa75134007deceef0807ea6
pnija <nijap@techversantinfotech.com>
2018-01-08 18:10:44 +0530

    on delete action added

--- apps/bitcoin_crypto/models.py
+++ apps/bitcoin_crypto/models.py
@@ -2,7 +2,7 @@ from apps.authentication.models import User
 from django.db import models
 
 class Transaction(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     from_currency = models.CharField(blank=False, max_length=10)
     to_currency = models.CharField(blank=False, max_length=10)
     amount = models.CharField(blank=False, max_length=200)
